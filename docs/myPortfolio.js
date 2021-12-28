b73fa8c35be6a411c9d65e945e010c45ff1f1b38
Carol0730 <53885675+Carol0730@users.noreply.github.com>
2021-12-28 21:08:56 +0800

    add hamburger

--- docs/myPortfolio.js
+++ docs/myPortfolio.js
@@ -1,1 +1,18 @@
 'use strict';
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
